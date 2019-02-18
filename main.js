4550114cbedf9831f1f9c080699e8b9adc1bcd5e
Martynas Rimselis <rimselis@gmail.com>
2019-02-18 09:45:20 +0200

    disabled subscriptions part to allow parser testing

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const { extractTransactions } = require('./parser');
-const { extractSubscriptions } = require('./subscriptions');
+// const { extractSubscriptions } = require('./subscriptions');
 
 Promise.all([
   extractTransactions('SEB_EXPORT_DEV_0003.xml', /\s*txnx\s*(VI|ST|OC|FL|1OFF)\s*(\d+)\s*/i),
-  extractSubscriptions('INITIAL_SUBSCRIPTIONS.json')
+  // extractSubscriptions('INITIAL_SUBSCRIPTIONS.json')
 ])
   .then(([data, subscriptions]) => {
     const transactions = [...data].sort((tnx1, tnx2) => tnx1.unixDateTime - tnx2.unixDateTime);
 
-    const unprocessedPayments =
-      transactions.filter(tnx => !subscriptions.processedPayments.includes(tnx.tnxId));
+    const unprocessedPayments = subscriptions
+      ? transactions.filter(tnx => !subscriptions.processedPayments.includes(tnx.tnxId))
+      : transactions;
 
     unprocessedPayments.map(tnx => console.log(tnx));
   });
